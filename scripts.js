d44bd24bf8f66edea0a88cb6ee0eab31475fe48d
joshuadavidjameswalker <108343789+joshuadavidjameswalker@users.noreply.github.com>
2023-04-06 15:08:14 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -37,7 +37,6 @@ function populateLoopToOptions() {
         loopToOptions.add(option);
     });
 }
-
 nextStepBtn.addEventListener("click", () => {
     appendNode(inputText.value);
     if (nodeId > 2) {
@@ -58,9 +57,6 @@ confirmLoopBtn.addEventListener("click", () => {
     const selectedNodeId = loopToOptions.value;
     flowchartData += `A${nodeId - 1}-->${selectedNodeId};`;
     updateFlowchart();
-    loopToSelector.style.display = "none";
-});
-
 choiceBtn.addEventListener("click", () => {
     choiceTextYes.value = "";
     choiceTextNo.value = "";
@@ -83,49 +79,48 @@ decisionBtn.addEventListener("click", () => {
     choiceTextContainer.style.display = "none";
     decisionBtn.style.display = "none";
 });
-
 saveAsImageBtn.addEventListener("click", () => {
     const svgElement = flowchartContainer.querySelector("svg");
     if (svgElement) {
         saveSvgAsPng(svgElement, "flowchart.png");
     }
-});
-
 deleteNodeBtn.addEventListener("click", () => {
     if (nodeId > 1) {
         const removedNode = nodes.pop();
-        flowchartData = flowchartchartData.replace(new RegExp(${removedNode.id}\\("(.+?)"\\);, "g"), "");
-flowchartData = flowchartData.replace(new RegExp(A${nodeId - 2}-->.*?${removedNode.id};, "g"), "");
-nodeId--;
-updateFlowchart();
-}
+        flowchartData = flowchartData.replace(new RegExp(`${removedNode.id}\\("(.+?)"\\);`, "g"), "");
+        flowchartData = flowchartData.replace(new RegExp(`A${nodeId - 2}-->.*?${removedNode.id};`, "g"), "");
+        nodeId--;
+        updateFlowchart();
+    }
 });
 
 // Add event listeners for highlighting the selected button
 [nextStepBtn, loopToBtn, choiceBtn].forEach(btn => {
-btn.addEventListener("click", () => {
-[nextStepBtn, loopToBtn, choiceBtn].forEach(b => b.classList.remove("selected"));
-btn.classList.add("selected");
-});
+    btn.addEventListener("click", () => {
+        [nextStepBtn, loopToBtn, choiceBtn].forEach(b => b.classList.remove("selected"));
+        btn.classList.add("selected");
+    });
 });
 
 // Save as Image implementation (Add this to the bottom of the script)
 function saveSvgAsPng(svgElement, fileName) {
-const svgData = new XMLSerializer().serializeToString(svgElement);
-const canvas = document.createElement("canvas");
-const ctx = canvas.getContext("2d");
-const img = new Image();
-img.setAttribute("src", "data:image/svg+xml;base64," + btoa(svgData));
+    const svgData = new XMLSerializer().serializeToString(svgElement);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.setAttribute("src", "data:image/svg+xml;base64," + btoa(svgData));
+
     img.onload = function() {
-    canvas.width = img.width;
-    canvas.height = img.height;
-   
-    ctx.drawImage(img, 0, 0);
-    const imgURL = canvas.toDataURL("image/png");
-    const link = document.createElement("a");
-    link.href = imgURL;
-    link.download = fileName;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-};
+        canvas.width = img.width;
+        canvas.height = img.height;
+       
+        ctx.drawImage(img, 0, 0);
+        const imgURL = canvas.toDataURL("image/png");
+        const link = document.createElement("a");
+        link.href = imgURL;
+        link.download = fileName;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+}
