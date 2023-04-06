12e8826ddc86e43b27aaf6ae0b77531ed567b555
joshuadavidjameswalker <108343789+joshuadavidjameswalker@users.noreply.github.com>
2023-04-06 18:36:14 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,38 +1,53 @@
 const inputText = document.getElementById("inputText");
 const nextStepBtn = document.getElementById("nextStepBtn");
-const addLoopBtn = document.getElementById("addLoopBtn");
-const addDecisionPointBtn = document.getElementById("addDecisionPointBtn");
+const loopBtn = document.getElementById("loopBtn");
+const choiceBtn = document.getElementById("choiceBtn");
 const choiceTextContainer = document.getElementById("choiceTextContainer");
 const choiceTextYes = document.getElementById("choiceTextYes");
 const choiceTextNo = document.getElementById("choiceTextNo");
 const decisionBtn = document.getElementById("decisionBtn");
-const deleteNodeBtn = document.getElementById("deleteNodeBtn");
+const loopSelect = document.getElementById("loopSelect");
+const loopText = document.getElementById("loopText");
 const saveAsImageBtn = document.getElementById("saveAsImageBtn");
+const deleteNodeBtn = document.getElementById("deleteNodeBtn");
+const nodesContainer = document.getElementById("nodesContainer");
 const flowchart = document.getElementById("flowchart");
 
-let nodeId = 0;
+let nodeId = 1;
 let flowchartData = "";
 
+mermaid.initialize({ startOnLoad: true });
+
 function updateFlowchart() {
-    flowchart.innerHTML = flowchartData;
-    // Add your preferred flowchart library to render the flowchart
+    flowchart.innerHTML = `graph LR
+        ${flowchartData}
+    `;
+    mermaid.init(undefined, flowchart);
 }
 
 function appendNode(text) {
-    flowchartData += `A${nodeId++}->|${text}|A${nodeId};`;
+    nodesContainer.innerHTML += `<div>A${nodeId}: ${text}</div>`;
+    nodeId++;
 }
 
 nextStepBtn.addEventListener("click", () => {
     appendNode(inputText.value);
+    flowchartData += `A${nodeId - 2}-->A${nodeId - 1};`;
     updateFlowchart();
 });
 
-addLoopBtn.addEventListener("click", () => {
-    // Add your loop creation logic here
+loopBtn.addEventListener("click", () => {
+    choiceTextContainer.style.display = "none";
+    decisionBtn.style.display = "none";
+    loopSelect.style.display = "block";
+    loopText.style.display = "block";
 });
 
-addDecisionPointBtn.addEventListener("click", () => {
+choiceBtn.addEventListener("click", () => {
     choiceTextContainer.style.display = "block";
+    decisionBtn.style.display = "block";
+    loopSelect.style.display = "none";
+    loopText.style.display = "none";
 });
 
 decisionBtn.addEventListener("click", () => {
@@ -48,12 +63,37 @@ decisionBtn.addEventListener("click", () => {
 
     updateFlowchart();
     choiceTextContainer.style.display = "none";
+    decisionBtn.style.display = "none";
 });
 
-deleteNodeBtn.addEventListener("click", () => {
-    // Add your node deletion logic here
+loopSelect.addEventListener("change", () => {
+    const loopToNodeId = loopSelect.value;
+    const loopLineText = loopText.value || "Loop";
+    flowchartData += `A${nodeId - 1}-->|${loopLineText}|A${loopToNodeId};`;
+    updateFlowchart();
+    loopSelect.style.display = "none";
+    loopText.style.display = "none";
 });
 
 saveAsImageBtn.addEventListener("click", () => {
-    // Add your save as image logic here
+    const svgElement = flowchart.querySelector("svg");
+    const svgData = new XMLSerializer().serializeToString(svgElement);
+    const svgBlob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
+    const svgUrl = URL.createObjectURL(svgBlob);
+
+    const link = document.createElement("a");
+    link.href = svgUrl;
+    link.download = "flowchart.svg";
+    link.click();
 });
+
+deleteNodeBtn.addEventListener("click", () => {
+    if (nodeId > 1) {
+        nodesContainer.removeChild(nodesContainer.lastChild);
+        nodeId--;
+        flowchartData = flowchartData.split(';').slice(0, -1).join(';') + ';';
+        updateFlowchart();
+    }
+});
+
+updateFlowchart();
