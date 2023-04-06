988fd17a999f6e413b318fce97ea42ff2935c0a0
joshuadavidjameswalker <108343789+joshuadavidjameswalker@users.noreply.github.com>
2023-04-06 18:56:09 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,65 +1,73 @@
-const mermaidDiv = document.querySelector(".mermaid");
-const inputText = document.getElementById("inputText");
-const choiceTextYes = document.getElementById("choiceTextYes");
-const choiceTextNo = document.getElementById("choiceTextNo");
-const loopText = document.getElementById("loopText");
-const loopToNode = document.getElementById("loopToNode");
-const nextStepBtn = document.getElementById("nextStepBtn");
-const addDecisionPointBtn = document.getElementById("addDecisionPointBtn");
-const addLoopBtn = document.getElementById("addLoopBtn");
-const deleteNodeBtn = document.getElementById("deleteNodeBtn");
-const saveAsImageBtn = document.getElementById("saveAsImageBtn");
+const nodes = [];
+const connections = [];
 
-let flowchartData = "graph LR;";
-let nodeId = 1;
-
-function updateFlowchart() {
-    mermaidDiv.innerHTML = flowchartData;
-    mermaid.init(undefined, mermaidDiv);
+function appendNode(text) {
+  const node = {
+    id: nodes.length + 1,
+    text: text,
+  };
+  nodes.push(node);
 }
 
-function appendNode(text, id) {
-    flowchartData += `${id || "A" + nodeId}("${text}");`;
-    nodeId++;
+function addConnection(source, target, label) {
+  connections.push({ source, target, label });
+}
+
+function updateFlowchart() {
+  let flowchartData = "graph LR;\n";
+  for (const node of nodes) {
+    flowchartData += `A${node.id}("${node.text}");\n`;
+  }
+
+  for (const connection of connections) {
+    flowchartData += `A${connection.source}-->|${connection.label}|A${connection.target};\n`;
+  }
+
+  document.querySelector("#flowchart").innerHTML = flowchartData;
+  mermaid.init(undefined, "#flowchart");
 }
 
 nextStepBtn.addEventListener("click", () => {
-    appendNode(inputText.value);
-    updateFlowchart();
+  appendNode(inputText.value);
+  if (nodes.length > 1) {
+    addConnection(nodes.length - 1, nodes.length, "");
+  }
+  updateFlowchart();
+});
+
+decisionBtn.addEventListener("click", () => {
+  const yesText = choiceTextYes.value || "Yes";
+  const noText = choiceTextNo.value || "No";
+
+  appendNode(inputText.value);
+  addConnection(nodes.length - 1, nodes.length, yesText);
+  appendNode(yesText);
+  addConnection(nodes.length - 1, nodes.length, "");
+
+  appendNode(noText);
+  addConnection(nodes.length - 3, nodes.length, noText);
+  updateFlowchart();
+  choiceTextContainer.style.display = "none";
+  decisionBtn.style.display = "none";
 });
 
 addDecisionPointBtn.addEventListener("click", () => {
-    const yesText = choiceTextYes.value || "Yes";
-    const noText = choiceTextNo.value || "No";
-    appendNode(inputText.value);
-    flowchartData += `A${nodeId - 1}-->|${yesText}|A${nodeId};`;
-    appendNode(yesText);
-    flowchartData += `A${nodeId - 2}-->|${noText}|A${nodeId};`;
-    appendNode(noText);
-    updateFlowchart();
+  choiceTextContainer.style.display = "block";
+  decisionBtn.style.display = "block";
 });
 
 addLoopBtn.addEventListener("click", () => {
-    const loopNodeId = loopToNode.value;
-    if (!loopNodeId) return;
-    flowchartData += `A${nodeId - 1}-->|${loopText.value || "Back"}|${loopNodeId};`;
+  const loopTo = parseInt(loopToNode.value);
+  if (loopTo >= 1 && loopTo < nodes.length) {
+    addConnection(nodes.length, loopTo, "");
     updateFlowchart();
+  }
 });
 
 deleteNodeBtn.addEventListener("click", () => {
-    flowchartData = flowchartData.replace(/A\d+\("\w*"\);/g, "").replace(/A\d+-->\|\w*\|A\d+;/g, "");
-    nodeId--;
+  if (nodes.length > 0) {
+    nodes.pop();
+    connections.pop();
     updateFlowchart();
-});
-
-saveAsImageBtn.addEventListener("click", () => {
-    const svg = mermaidDiv.querySelector("svg");
-    const serializer = new XMLSerializer();
-    const source = serializer.serializeToString(svg);
-    const img = new Image();
-    img.src = "data:image/svg+xml;base64," + btoa(source);
-    const link = document.createElement("a");
-    link.href = img.src;
-    link.download = "flowchart.png";
-    link.click();
+  }
 });
